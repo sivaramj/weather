ddb230daad1a5215dcbff9989e4b21fb85e2c493
Jebamani, Sivaram [GCB-OT NE] <sj03006@imcnam.ssmb.com>
2017-12-26 21:36:09 -0500

    Initial Project files

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Jebamani, Sivaram on 12/26/17.
+//  Copyright © 2017 Jebamani, Sivaram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
